aab594ac6f2201126eb0bf66ea07013d25722acb
cedric <cedricnettler@gmail.com>
2022-08-29 15:32:24 +0200

    Erstes Update

---
+++ Aufgabe_6_Algorithmen/main_algorithmus.c
@@ -0,0 +1,10 @@
+//      Autor:          Cedric-Julian Nettler
+//      Mail:           cedric-julian.nettler@stud.thga.de
+//      Matr. Nr.:      239563
+//      Datum:          29.08.2022
+//      Hausarbeit:     Aufgabe 6 "Suche nach längster Reihe nicht aufeinanderfolgender Ziffern"
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+
