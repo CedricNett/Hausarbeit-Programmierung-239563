d1219ec3048b5d2189326071c9dc8bc6de8fb44a
cedric <cedricnettler@gmail.com>
2022-08-29 15:57:27 +0200

    Erstes Update

---
+++ Aufgabe_7_Funktionsgenerator/main_function-generator.c
@@ -0,0 +1,13 @@
+//      Autor:          Cedric-Julian Nettler
+//      Mail:           cedric-julian.nettler@stud.thga.de
+//      Matr. Nr.:      239563
+//      Datum:          29.08.2022
+//      Hausarbeit:     Aufgabe 7 "Funktionsgenerator"
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <math.h>
+#include "header_help-catalogue.h"
+#include <static_function-generator.h>
