fbb73dc542706feea5930aeb03e4b8df33707452
cedric <cedricnettler@gmail.com>
2022-09-06 18:11:36 +0200

    Test Update

--- Aufgabe_6_Algorithmen/temp_Algorithmus.c
+++ Aufgabe_6_Algorithmen/temp_Algorithmus.c
@@ -10,52 +10,112 @@
 int main()
 {
 
-        FILE * zr;
-        zr = fopen("ziffernreihe.txt", "r");
+//  Hier wird geschaut ob die Zifferndatei geöffnet wurde
+    FILE * zr;
+    zr = fopen("ziffernreihe.txt", "r");
 
-        if(zr == NULL)
-        {
-                printf("Datei konnte NICHT geöffnet werden.\n");
-        }
-        else
-        {
-                printf("Datei konnte geöffnet werden.\n");
-        }
+    if(zr == NULL)
+    {
+        printf("Datei konnte NICHT geöffnet werden.\n");
+    }
+    else
+    {
+        printf("Datei konnte geöffnet werden.\n");
+    }
 
-	int a;
+// Hier werden die Ziffern der Datei mit Integers versehen um diese später zu nutzen
 
-        int ziffernreihe[50];
+    int a;
 
-       for (int i=0; i<50; i++)
-        {
-                fscanf(zr, " %i,", &ziffernreihe[i]);
+    int ziffernreihe[50];
 
-                printf(" i = %i -> j = %i \n", i, ziffernreihe[i]);
-	}
+    for (int i=0; i<50; i++)
+    {
+        fscanf(zr, " %i,", &ziffernreihe[i]);
 
-	printf("\n\n");
-
-		for (int i=0,j=1; i<50,j<50; i++,j++)
-		{
-			a = ziffernreihe[j] - ziffernreihe[i];
-
-                	printf("%i -> a = %i \n", j, a);
-
-                	if(a == 1)
-                	{
-                        	printf("return 1 \n\n");
-                	}
-
-                	else
-                	{
-                        	printf("return 0 \n\n");
-                	}
-		}	
-        
+        printf(" i = %i -> j = %i \n", i, ziffernreihe[i]);
+    }
 
     int stelle_max = 0;
     int max = 0;
 
-	fclose(zr);
+        for (int i=0; i<50; i++)
+    {
+       // a = ziffernreihe[j] - ziffernreihe[i];
+
+        if(i<50)
+        {
+	int x = 0;
+
+	for (int j=1; x<1; j++)
+	{
+		
+            if(j>max)
+            {
+                    max = j;
+
+                    stelle_max = i;
+            }
+
+            i = i + j;
+
+            x = 1;
+
+            printf(" Hier sind aufeinanderfolgende Zahlen \n");
+        }
+
+    //   else
+    //    {
+    //        printf(" Hier sind keine aufeinanderfolgende Zahlen \n");
+    //    }
+	}
+    }
+/*    for (int i=0,j=1; i<50, x<1; i++,j++)
+    {
+        a = ziffernreihe[j] - ziffernreihe[i];
+
+        printf("\n j - i = %i -> a = %i \n", j, a);
+
+        if(a == 1)
+        {
+	    if(j>max)
+	    {
+		    max = j;
+
+		    stelle_max = i;
+	    }
+
+	    i = i + j;
+
+	    x = 1;
+
+            printf(" Hier sind aufeinanderfolgende Zahlen \n");
+        }
+	
+        else
+        {
+            printf(" Hier sind keine aufeinanderfolgende Zahlen \n");
+        }
+
+    } */
+
+    if (max < 2)
+    {
+	    printf("\n In der Zahlenreihe gibt es keine nicht aufeinanderfolgenden Ziffern! \n");
+    }
+
+    else
+    {
+    	printf("\n Die längste Reihe von nicht aufeinanderfolgenden Ziffern ist %i Ziffern lang.\n", max);
+    	printf("\n Die Ziffern lauten: ");
+
+    	for(int i=stelle_max; i < stelle_max+max; i++)
+    	{
+	    printf("%i, ", ziffernreihe[i]);
+        }
+        printf("\n");
+    }
+
+    fclose(zr);
     return 0;
 }
